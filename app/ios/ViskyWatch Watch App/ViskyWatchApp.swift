933b64eb6dd00d24eb75c544263b12099a26d768
neoff <en.varg@gmail.com>
2025-04-22 07:27:21 +0300

    #15 add github actions

---
+++ app/ios/ViskyWatch Watch App/ViskyWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViskyWatchApp.swift
+//  ViskyWatch Watch App
+//
+//  Created by Evgen Nesterov on 21.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ViskyWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
