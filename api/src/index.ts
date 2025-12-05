76344444eb92f63e3bddff1f1ea37c2555056fc5
neoff <en.varg@gmail.com>
2025-12-05 19:34:26 +0200

    fix: bind Express to 0.0.0.0 for IPv4 compatibility in k8s

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,7 +5,7 @@ import app from "@/router";
 //export * from "./helpers/strategies";
 
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   const { address, port } = server.address() as AddressInfo;
   const ip = address === '::' || address === '::1' ? '127.0.0.1' : address;
   console.log(`Listening at http://${ip}:${port}`)
